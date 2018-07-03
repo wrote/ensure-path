b97c55a623f86b998c88101ae87f147e13611f33
Anton <anton@adc.sh>
2018-07-03 10:15:12 +0100

    tests + context

--- test/context/index.js
+++ test/context/index.js
@@ -1,33 +1,76 @@
 import { resolve } from 'path'
-import { debuglog } from 'util'
+import makePromise from 'makepromise'
+import { mkdir, createWriteStream, createReadStream } from 'fs'
+import spawn from 'spawncommand'
+import Catchment from 'catchment'
+import { equal } from 'assert'
+// import { debuglog } from 'util'
 
-const LOG = debuglog('@wrote/ensure-path')
+// const LOG = debuglog('@wrote/ensure-path')
 
-const FIXTURE = resolve(__dirname, '../fixture')
+const TEMP_DIR = resolve(__dirname, '../temp')
+const TEST_DIR_NAME = '_tests'
+const TEST_DIR_NOX_NAME = 'no-execute'
 
 /**
  * A testing context for the package.
  */
 export default class Context {
   async _init() {
-    LOG('init context')
+    try {
+      const { promise } = spawn('rm', ['-rf', this.TEMP_TEST_DIR])
+      await promise
+      await makePromise(mkdir, [this.TEMP_TEST_DIR, 0o777])
+    } catch (err) {
+      if (/EEXIST/.test(err.message)) {
+        throw new Error('WroteContext: Could not make temp test directory: it already exists.')
+      }
+      throw err
+    }
   }
   /**
-   * Example method.
+   * Returns path to the test directory in the temp folder.
    */
-  example() {
-    return 'OK'
+  get TEMP_TEST_DIR() {
+    return resolve(TEMP_DIR, TEST_DIR_NAME)
   }
   /**
-   * Path to the fixture file.
+   * Returns path to the non-executable directory in the temp folder.
    */
-  get FIXTURE() {
-    return resolve(FIXTURE, 'test.txt')
+  get TEMP_NOX_DIR() {
+    return resolve(TEMP_DIR, TEST_DIR_NOX_NAME)
   }
-  get SNAPSHOT_DIR() {
-    return resolve(__dirname, '../snapshot')
+  /**
+   * Create a non-executable directory in the temp folder.
+   */
+  async makeNoExecutableDirectory() {
+    try {
+      await makePromise(mkdir, [this.TEMP_NOX_DIR, 0o666])
+    } catch ({ message }) {
+      if (/EEXIST/.test(message)) {
+        throw new Error('WroteContext: Could not make non-executable directory: it already exists.')
+      }
+    }
   }
   async _destroy() {
-    LOG('destroy context')
+    const promises = []
+    const { promise: p1 } = spawn('rm', ['-rf', this.TEMP_TEST_DIR])
+    promises.push(p1)
+    const { promise: p2 } = spawn('rm', ['-rf', this.TEMP_NOX_DIR])
+    promises.push(p2)
+    await Promise.all(promises)
+  }
+
+  async assertCanWriteFile(path) {
+    const testData = `some-test-data-${Date.now()}`
+    const ws = createWriteStream(path)
+    await new Promise((r) => {
+      ws.end(testData, r)
+    })
+    const rs = createReadStream(path)
+
+    const { promise } = new Catchment({ rs })
+    const res = await promise
+    equal(res, testData)
   }
 }
--- test/spec/default.js
+++ test/spec/default.js
@@ -1,21 +1,41 @@
-import { equal, ok } from 'zoroaster/assert'
+import { equal, throws } from 'zoroaster/assert'
+import { resolve } from 'path'
 import Context from '../context'
-import wroteEnsurePath from '../../src'
+import ensurePath from '../../src'
 
 /** @type {Object.<string, (c: Context)>} */
 const T = {
   context: Context,
   'is a function'() {
-    equal(typeof wroteEnsurePath, 'function')
+    equal(typeof ensurePath, 'function')
   },
-  async 'calls package without error'() {
-    await wroteEnsurePath()
+  async 'creates a path to the file'({ assertCanWriteFile, TEMP_TEST_DIR }) {
+    const path = resolve(TEMP_TEST_DIR, 'path/to/temp/file.data')
+    const res = await ensurePath(path)
+    equal(res, path)
+    await assertCanWriteFile(path)
   },
-  async 'gets a link to the fixture'({ FIXTURE }) {
-    const res = await wroteEnsurePath({
-      type: FIXTURE,
+  async 'rejects when trying to create a path where cannot'({
+    makeNoExecutableDirectory, TEMP_NOX_DIR,
+  }) {
+    await makeNoExecutableDirectory()
+    const path = resolve(TEMP_NOX_DIR, 'path/to/temp/file.data')
+    await throws({
+      async fn() {
+        await ensurePath(path)
+      },
+      code: 'EACCES',
+      message: /permission denied/,
     })
-    ok(res, FIXTURE)
+  },
+  async 'does not throw an error when dir already exists'({
+    assertCanWriteFile, TEMP_TEST_DIR,
+  }) {
+    const path = resolve(TEMP_TEST_DIR, 'file.data')
+    const res = await ensurePath(path)
+    equal(res, path)
+    await assertCanWriteFile(path)
+    await ensurePath(path)
   },
 }
 
