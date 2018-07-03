f55ade035ff350f75353d9924ce5052660c0ec6d
Anton <anton@adc.sh>
2018-07-03 12:54:57 +0100

    build

--- build/index.js
+++ build/index.js
@@ -3,23 +3,67 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = wroteEnsurePath;
+exports.default = ensurePath;
 
-var _util = require("util");
+var _fs = require("fs");
 
-const LOG = (0, _util.debuglog)('@wrote/ensure-path');
+var _makepromise = _interopRequireDefault(require("makepromise"));
+
+var _path = require("path");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 /**
- * Create all directories on the way to the path.
- * @param {Config} config Configuration object.
- * @param {string} config.type The type.
+ * Make sure that a file can be created by creating all directories to which it belongs, e.g., `ensurePath('/usr/local/test/wrote.data')` will attempt to create `/usr/local/test/` directory recursively starting from `usr`.
+ * @param {string} path Path to the file
+ * @returns {Promise.<string>} Same path as passed
+ * @throws {Error} When the first folder in the path is non-executable
+ */
+async function ensurePath(path) {
+  const dir = (0, _path.dirname)(path);
+
+  try {
+    await make(dir);
+    return path;
+  } catch (err) {
+    if (/EEXIST/.test(err.message) && err.message.indexOf(dir) != -1) {
+      return path;
+    }
+
+    throw err;
+  }
+}
+/**
+ * Recursive promise-based mkdir.
+ * @param {string} dir Path to the directory to be created
  */
 
-async function wroteEnsurePath(config = {}) {
-  const {
-    type
-  } = config;
-  LOG('@wrote/ensure-path called with %s', type);
-  return type;
+
+async function make(dir) {
+  try {
+    const res = await makeDir(dir);
+    return res;
+  } catch (err) {
+    if (/ENOENT/.test(err.message)) {
+      const parentDir = (0, _path.dirname)(dir);
+      await make(parentDir);
+      const res2 = await make(dir);
+      return res2;
+    }
+
+    throw err;
+  }
+}
+/**
+ * Promisified fs.mkdir
+ * @param {string} dir directory name
+ * @returns {string} created directory name
+ */
+
+
+async function makeDir(dir) {
+  const res = await (0, _makepromise.default)(_fs.mkdir, dir, dir);
+  return res;
 }
 /**
  * @typedef {Object} Config
